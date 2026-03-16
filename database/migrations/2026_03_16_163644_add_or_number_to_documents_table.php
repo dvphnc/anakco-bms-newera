e71672d04a6ee48ef043b44c1d1adf44807e62e9
Joana Daphne Sy <jdsoffcl@gmail.com>
2026-03-17 00:37:13 +0800

    auto: update database/migrations/2026_03_16_163644_add_or_number_to_documents_table.php

--- database/migrations/2026_03_16_163644_add_or_number_to_documents_table.php
+++ database/migrations/2026_03_16_163644_add_or_number_to_documents_table.php
@@ -6,23 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::table('documents', function (Blueprint $table) {
-            //
+            $table->string('or_number')->nullable()->after('fee_paid');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::table('documents', function (Blueprint $table) {
-            //
+            $table->dropColumn('or_number');
         });
     }
 };
