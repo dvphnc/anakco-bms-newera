74bcb39ec2a88fc6dec41e22df11287588fc6a2b
Joana Daphne Sy <jdsoffcl@gmail.com>
2026-03-16 15:25:08 +0800

    auto: update resources/views/dashboard/dashboard.blade.php

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -341,8 +341,8 @@
         <div class="card-body">
             @php
                 $committeeList = [
-                    ['slug' => 'peace-order',    'name' => 'Peace & Order',      'icon' => 'fa-shield',      'color' => '#f85149'],
-                    ['slug' => 'health',         'name' => 'Health',             'icon' => 'fa-heart-pulse',         'color' => '#f78166'],
+                    ['slug' => 'peace-order',    'name' => 'Peace & Order',      'icon' => 'fa-shield',              'color' => '#f85149'],
+                    ['slug' => 'health',         'name' => 'Health',             'icon' => 'fa-heartbeat',         'color' => '#f78166'],
                     ['slug' => 'education',      'name' => 'Education',          'icon' => 'fa-graduation-cap',      'color' => '#79c0ff'],
                     ['slug' => 'infrastructure', 'name' => 'Infrastructure',     'icon' => 'fa-road',                'color' => '#e3b341'],
                     ['slug' => 'environment',    'name' => 'Environment',        'icon' => 'fa-leaf',                'color' => '#3fb950'],
