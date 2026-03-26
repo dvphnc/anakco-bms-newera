01acd016b05d02ab99abe35fc5e44bf469871b32
Joana Daphne Sy <jdsoffcl@gmail.com>
2026-03-26 14:39:41 +0800

    auto: Routes & General — route & config update

--- routes/web.php
+++ routes/web.php
@@ -121,7 +121,8 @@ Route::middleware(['auth', 'verified'])->group(function () {
     // ---------------------------------------------------
     Route::resource('users', UserController::class)
         ->middleware('role:Admin');
-    
+    Route::post('users/{user}/verify', [UserController::class, 'verify'])->name('users.verify')->middleware('role:Admin');
+    Route::post('users/{user}/unverify', [UserController::class, 'unverify'])->name('users.unverify')->middleware('role:Admin');
 
     // ---------------------------------------------------
     // Activity Log — Admin + Secretary
