998b35de08b6188d620f9757f7b6a221432802c2
Joana Daphne Sy <jdsoffcl@gmail.com>
2026-05-20 16:23:55 +0800

    auto: Layout — updated blade.php

--- resources/views/partials/_topbar.blade.php
+++ resources/views/partials/_topbar.blade.php
@@ -88,13 +88,10 @@
             </div>
         </div>
         <div class="topbar-divider"></div>
-        <form method="POST" action="{{ route('logout') }}">
-            @csrf
-            <button type="submit" class="topbar-btn">
-                <i class="fas fa-sign-out-alt"></i>
-                <span>Logout</span>
-            </button>
-        </form>
+        <button type="button" class="topbar-btn" onclick="bmsLogout()">
+            <i class="fas fa-sign-out-alt"></i>
+            <span>Logout</span>
+        </button>
     </div>
 </header>
 
