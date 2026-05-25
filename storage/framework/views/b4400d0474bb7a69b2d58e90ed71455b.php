fbcdc0a056489c6432a2cb5d2ae8d812cf130d3c
Joana Daphne Sy <jdsoffcl@gmail.com>
2026-05-25 15:11:51 +0800

    auto: General — update (5 files)

--- storage/framework/views/b4400d0474bb7a69b2d58e90ed71455b.php
+++
@@ -1,4 +0,0 @@
-<img src="<?php echo e(asset('images/bne-logo.png')); ?>"
-     alt="Official Seal of Barangay New Era"
-     style="width:100%;height:100%;object-fit:contain;display:block;">
-<?php /**PATH D:\laragon\www\anakco_bms\resources\views/partials/_portal_seal.blade.php ENDPATH**/ ?>
